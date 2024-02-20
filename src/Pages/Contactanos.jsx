4e4520df9dc4b023c012f248c2ae1e99ee511ffa
Lari <larihid1@gmail.com>
2024-02-20 01:04:19 -0600

    add: emailjs

--- src/Pages/Contactanos.jsx
+++ src/Pages/Contactanos.jsx
@@ -5,11 +5,20 @@ import SimpleMap from "../Components/Mapa"
 import { Nav } from "../Components/Nav"
 import { Subtitulos } from "../Components/Subtitulos"
 import ImgContactanos from '../assets/icon/ImgNosotros.svg'
-
+import emailjs from 'emailjs-com'
 
 export const Contactanos = () => {
 
     const [selectedValue, setSelectedValue] = useState("value1");
+    
+    const enviarEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_tr8emmu','template_ads0sty',e.target,'-AEnOZ1NIz3dBVu0E').then(res=>{
+            console.log(res);
+            alert("Correo enviado")
+        })
+    }
 
     return(
         <>
@@ -33,21 +42,21 @@ export const Contactanos = () => {
                         </div>
 
                     </div>
-                    <div className="flex flex-col gap-5">
+                    <form className="flex flex-col gap-5" onSubmit={enviarEmail}>
                         <div className="flex flex-row gap-14">
                             <div className="flex flex-col">
                                 <label className="text-[#397A80] font-medium text-sm">Nombre(s)</label>
-                                <input type="text" placeholder="Nombre(s)" className="w-60 h-12 bg-[#F3F3F3] pl-2" />
+                                <input type="text" placeholder="Nombre(s)" id="nombre"  name='nombre' className="w-60 h-12 bg-[#F3F3F3] pl-2" />
                             </div>
                             <div className="flex flex-col">
                                 <label className="text-[#397A80] font-medium text-sm">Apellidos</label>
-                                <input type="text" placeholder="Apellidos" className="w-60 h-12 bg-[#F3F3F3] pl-2" />
+                                <input type="text" placeholder="Apellidos" id="apellido" name= "apellido" className="w-60 h-12 bg-[#F3F3F3] pl-2" />
                             </div>
                         </div>
                         <div className="flex flex-row gap-14">
                             <div className="flex flex-col">
                                 <label className="text-[#397A80] font-medium text-sm">Tipo de servicio</label>
-                                <select value={selectedValue} onChange={(e) => setSelectedValue(e.target.value)} className="w-60 h-12 bg-[#F3F3F3] text-[#768485] pl-2">
+                                <select value={selectedValue} onChange={(e) => setSelectedValue(e.target.value)} id="servicio" name="servicio" className="w-60 h-12 bg-[#F3F3F3] text-[#768485] pl-2">
                                     <option value="Seleccionar" disabled defaultValue="Seleccionar">--Seleccionar--</option>
                                     <option value="auditoria" className="">Auditoría</option>
                                     <option value="consultoria" className="">Consultoría</option>
@@ -56,27 +65,27 @@ export const Contactanos = () => {
                             </div>
                             <div className="flex flex-col">
                                 <label className="text-[#397A80] font-medium text-sm">Teléfono</label>
-                                <input type="tel" placeholder="Teléfono" className="w-60 h-12 bg-[#F3F3F3] pl-2" />
+                                <input type="tel" placeholder="Teléfono" id="tel" name="tel" className="w-60 h-12 bg-[#F3F3F3] pl-2" />
                             </div>
                         </div>
                         <div className="flex flex-row">
                             <div className="flex flex-col">
                                 <label className="text-[#397A80] font-medium text-sm">Correo Electrónico</label>
-                                <input type="email" placeholder="Correo Electrónico" className="w-[537px] h-12 bg-[#F3F3F3] pl-2" />
+                                <input type="email" placeholder="Correo Electrónico" id="email" name="email" className="w-[537px] h-12 bg-[#F3F3F3] pl-2" />
                             </div>
                         </div>
                         <div className="flex flex-row">
                             <div className="flex flex-col">
                                 <label className="text-[#397A80] font-medium text-sm">Mensaje</label>
-                                <textarea placeholder="Mensaje" className="w-[537px] h-44 bg-[#F3F3F3] pl-2 pt-2">
+                                <textarea placeholder="Mensaje" id="mensaje" name="mensaje" className="w-[537px] h-44 bg-[#F3F3F3] pl-2 pt-2">
 
                                 </textarea>
                             </div>
                         </div>
                         <div className="flex justify-center">
-                            <button className="bg-[#397A80] text-[#FFFFFF] w-48 h-12 rounded-[10px] font-bold text-base">Enviar</button>
+                            <button onSubmit={enviarEmail} className="bg-[#397A80] text-[#FFFFFF] w-48 h-12 rounded-[10px] font-bold text-base">Enviar</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
 
